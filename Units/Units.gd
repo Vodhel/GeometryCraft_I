53c6a6c4f78dfa49e643bf0a1291ecf40639b28d
Vodhel <alexandrev1700@gmail.com>
2024-03-04 19:37:30 -0500

    Created some sort of structure, trying to do some base class for the units

---
+++ Units/Units.gd
@@ -0,0 +1,17 @@
+extends Node2D
+class_name Unit 
+
+@export var hp 		 := 100.0
+@export var velocity := 1.0
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if hp == 0:
+		queue_free()
